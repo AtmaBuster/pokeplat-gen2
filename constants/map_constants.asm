1dc23e61b1ca41a6fa2c4bb61d90d1d78e965a2a
TastySnax12 <tastysnax12@gmail.com>
2021-07-31 13:58:39 -0400

    Various map changes.

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -480,7 +480,7 @@ ENDM
 
 	newgroup                                                      ; 26
 
-	map_const ROUTE_30,                                    10, 27 ;  1
+	map_const ROUTE_30,                                    12, 10 ;  1
 	map_const ROUTE_31,                                    20,  9 ;  2
 	map_const CHERRYGROVE_CITY,                            20,  9 ;  3
 	map_const CHERRYGROVE_MART,                             6,  4 ;  4
@@ -491,3 +491,5 @@ ENDM
 	map_const ROUTE_30_BERRY_HOUSE,                         4,  4 ;  9
 	map_const MR_POKEMONS_HOUSE,                            4,  4 ; 10
 	map_const ROUTE_31_VIOLET_GATE,                         5,  4 ; 11
+	
+	
