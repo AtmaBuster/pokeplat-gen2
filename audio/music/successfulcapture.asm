99b10c65b600658a18cf288b1d300c75828cf3a6
U-TASTYSNAX12-X1-\TastySnax12 <tastysnax12@gmail.com>
2022-01-15 15:31:31 -0500

    Added new Wild Victory/Capture music.

--- audio/music/successfulcapture.asm
+++ audio/music/successfulcapture.asm
@@ -4,19 +4,247 @@ Music_SuccessfulCapture:
 	musicheader 1, 3, Music_SuccessfulCapture_Ch3
 
 Music_SuccessfulCapture_Ch1:
-	tempo 126
+	tempo 104
 	volume $77
-	dutycycle $3
-	tone $0001
-	notetype $c, $a1
-	jumpchannel Music_SuccessfulCapture_branch_f4536
+	notetype $C, $A2
+	dutycycle 2
+	octave 2
+	note E_, 4
+	octave 1
+	note B_, 4
+	octave 2
+	note E_, 2
+	note G#, 4
+	note E_, 4
+	note A_, 4
+	note E_, 2
+	note G#, 4
+	note E_, 4
+	note E_, 4
+	octave 1
+	note B_, 4
+	octave 2
+	note E_, 2
+	note G#, 4
+	note E_, 4
+	note A_, 4
+	note E_, 2
+	note G#, 4
+	note G_, 4
+	note F_, 4
+	note C_, 4
+	note F_, 2
+	note A_, 4
+	note F_, 4
+	note A#, 4
+	note F_, 2
+	note A_, 4
+	note F_, 4
+	note F_, 4
+	note C_, 4
+	note F_, 2
+	note A_, 4
+	note F_, 4
+	note A#, 4
+	note F_, 2
+	note A_, 4
+	note B_, 4
+	.loop
+
 
 Music_SuccessfulCapture_Ch2:
-	vibrato $12, $24
-	notetype $c, $c1
-	dutycycle $2
-	jumpchannel Music_SuccessfulCapture_branch_f4585
+	notetype $C, $B2
+	dutycycle 2
+	octave 4
+	intensity $B2
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note A_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 4
+	octave 5
+	note C#, 2
+	note C#, 2
+	note C#, 4
+	octave 4
+	note B_, 2
+	note B_, 2
+	note B_, 4
+	note B_, 2
+	note A_, 2
+	note G#, 2
+	note A_, 2
+	note B_, 2
+	note B_, 2
+	note B_, 4
+	octave 5
+	note C#, 2
+	note C#, 2
+	note C#, 4
+	octave 4
+	note B_, 8
+	octave 5
+	note C_, 2
+	octave 4
+	note A#, 2
+	note A_, 2
+	note A#, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	note C_, 4
+	note D_, 2
+	note D_, 2
+	note D_, 4
+	note C_, 2
+	note C_, 2
+	note C_, 4
+	note C_, 2
+	octave 4
+	note A#, 2
+	note A_, 2
+	note A#, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	note C_, 4
+	note D_, 2
+	note D_, 2
+	note D_, 4
+	note C_, 8
+	.loop
 
 Music_SuccessfulCapture_Ch3:
-	notetype $c, $25
-	jumpchannel Music_SuccessfulCapture_branch_f45cf
+	notetype $6, $12
+	octave 5
+	note G#, 3
+	note __, 1
+	note E_, 3
+	note __, 1
+	octave 4
+	note B_, 3
+	note __, 1
+	octave 5
+	note E_, 3
+	note __, 1
+	note G#, 3
+	note __, 1
+	octave 4
+	note B_, 3
+	note __, 1
+	octave 5
+	note E_, 3
+	note __, 5
+	note A_, 3
+	note __, 1
+	note C#, 3
+	note __, 1
+	note E_, 3
+	note __, 5
+	note G#, 3
+	note __, 1
+	octave 4
+	note B_, 3
+	note __, 1
+	octave 5
+	note E_, 3
+	note __, 5
+	note G#, 3
+	note __, 1
+	note E_, 3
+	note __, 1
+	octave 4
+	note G#, 3
+	note __, 1
+	note B_, 3
+	note __, 1
+	octave 5
+	note E_, 3
+	note __, 1
+	note G#, 3
+	note __, 1
+	octave 4
+	note B_, 3
+	note __, 5
+	octave 5
+	note E_, 3
+	note __, 1
+	note A_, 3
+	note __, 1
+	note C#, 3
+	note __, 5
+	note E_, 3
+	note __, 1
+	note D#, 3
+	note __, 1
+	note D_, 3
+	note __, 1
+	note C#, 3
+	note __, 1
+	note F_, 3
+	note __, 1
+	note C_, 3
+	note __, 1
+	octave 4
+	note A_, 3
+	note __, 1
+	octave 5
+	note C_, 3
+	note __, 1
+	note F_, 3
+	note __, 1
+	octave 4
+	note A_, 3
+	note __, 1
+	octave 5
+	note C_, 3
+	note __, 5
+	note F_, 3
+	note __, 1
+	octave 4
+	note A#, 3
+	note __, 1
+	octave 5
+	note D_, 3
+	note __, 5
+	note F_, 3
+	note __, 1
+	octave 4
+	note A_, 3
+	note __, 1
+	octave 5
+	note C_, 3
+	note __, 5
+	note F_, 3
+	note __, 1
+	note C_, 3
+	note __, 1
+	octave 4
+	note A_, 3
+	note __, 1
+	octave 5
+	note C_, 3
+	note __, 1
+	note F_, 3
+	note __, 1
+	note A_, 3
+	note __, 1
+	note C_, 3
+	note __, 5
+	note F_, 3
+	note __, 1
+	note A#, 3
+	note __, 1
+	note D_, 3
+	note __, 5
+	note F_, 3
+	note __, 1
+	note F_, 3
+	note __, 1
+	note F#, 3
+	note __, 1
+	note G_, 3
+	note __, 1
+	.loop
