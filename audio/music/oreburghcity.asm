1f13b4cd652251201fd0a9d1c8eb01193e622645
U-TASTYSNAX12-X1-\TastySnax12 <tastysnax12@gmail.com>
2022-01-16 12:39:06 -0500

    Updated Oreburgh City music.

--- audio/music/oreburghcity.asm
+++ audio/music/oreburghcity.asm
@@ -1,165 +1,242 @@
 Music_OreburghCity:
-	dbw $C0, Music_OreburghCity_Ch1
-	dbw $01, Music_OreburghCity_Ch2
-	dbw $02, Music_OreburghCity_Ch3
-	dbw $03, Music_OreburghCity_Ch4
+	channel_count 4
+	channel 1, Music_OreburghCity_Ch1
+	channel 2, Music_OreburghCity_Ch2
+	channel 3, Music_OreburghCity_Ch3
+	channel 4, Music_OreburghCity_Ch4
 
-Music_OreburghCity_Ch1:
-	tempo $D0
-	volume $77
-	dutycycle 0
-	tone $0001
-Music_OreburghCity_Ch1_loop:
-	notetype $C, $B2
+Music_OreburghCity_Ch1_sub_0:
 	octave 3
-	note A#, 3
-	note A#, 1
-	note A_, 1
-	note A#, 2
-	note A#, 2
-	octave 4
-	note D_, 2
-	note D_, 1
-	note C#, 1
-	note D_, 2
-	note D_, 1
-	note C_, 3
-	note C_, 1
-	octave 3
-	note B_, 1
-	octave 4
-	note C_, 2
-	octave 3
+	note G_, 3
+	note G_, 1
+	note E_, 3
 	note E_, 1
-	note D_, 2
-	note D_, 1
-	note D_, 2
-	note D_, 2
-	note D#, 3
-	note E_, 2
-	note C_, 4
-	note E_, 1
-	note E_, 2
-	note D_, 3
-	note C_, 2
-	note F_, 1
-	note F_, 2
-	octave 4
-	note C_, 2
-	note C_, 3
-	octave 3
-	note A#, 1
-	note A#, 2
-	note G#, 2
-	note G#, 3
-	intensity $B3
-	octave 4
-	note D_, 3
-	note D_, 3
-	note C_, 4
-	intensity $B1
-	octave 3
-	note B_, 2
-	note B_, 4
-	intensity $B3
-	octave 4
-	note C_, 3
-	note C_, 3
-	octave 3
-	note A#, 4
-	intensity $B1
-	note G#, 2
-	note G#, 6
-	intensity $B3
-	octave 2
-	note A_, 2
-	octave 3
-	note C_, 6
-	note D#, 2
-	note G_, 6
-	octave 2
 	note G_, 2
+	note G_, 1
 	note A#, 2
-	octave 3
-	note E_, 2
-	octave 4
-	note C_, 1
-	note C_, 1
-	octave 3
-	note B_, 1
-	note B_, 1
-	note A#, 1
-	note A#, 1
-	note A_, 1
-	note A_, 1
-	note C_, 1
-	note C_, 2
-	note C_, 2
-	note D_, 1
-	note C_, 2
-	note C_, 1
-	note C_, 2
-	note C_, 2
-	note D_, 1
-	note C_, 2
-	note C_, 1
-	note C_, 2
-	note C_, 2
-	note D_, 1
-	note C_, 1
 	note G_, 1
 	note E_, 1
-	note E_, 2
-	note A#, 2
-	note A#, 3
-	note A_, 1
-	note A_, 2
-	note A_, 2
+	note F_, 1
+	octave 4
+	note C_, 3
+	note C_, 1
+	octave 3
 	note A_, 3
 	note A_, 1
-	note A_, 2
-	note A_, 2
-	note A_, 2
-	note A_, 1
-	note G_, 2
-	note G_, 3
-	note G_, 3
-	note E_, 3
-	note G_, 5
-	intensity $B1
-	note G_, 2
-	note G_, 3
-	note G_, 3
-	note E_, 3
-	note G_, 2
-	note E_, 1
-	intensity $B3
-	note C_, 4
-	intensity $B1
-	note F_, 1
 	note F_, 2
-	note D#, 3
+	note F_, 1
+	note D_, 2
+	note D_, 1
+	octave 2
+	note A#, 1
+	sound_ret
+Music_OreburghCity_Ch1_sub_1:
+	octave 4
+	note C_, 1
+	octave 3
+	note F#, 1
+	note G_, 1
+	octave 4
+	note C_, 1
+	octave 3
+	sound_ret
+Music_OreburghCity_Ch1::
+	tempo 226
+	duty_cycle 3
+	vibrato 8, 1, 5
+	stereo_panning TRUE, FALSE
+.loop
+	pitch_offset 0
+	note_type 12, 11, 5
+	sound_call Music_OreburghCity_Ch1_sub_0
+	note B_, 1
+	sound_call Music_OreburghCity_Ch1_sub_0
+	octave 3
+	note D_, 1
+	note_type 12, 11, 2
+	note C#, 1
+	note D_, 1
+	note F#, 1
+	note D_, 2
+	note A_, 1
+	note F#, 1
+	note D_, 1
+	note F#, 1
+	note G_, 1
+	note B_, 1
+	note G_, 2
+	octave 4
+	note D#, 1
+	octave 3
+	note B_, 1
+	note G_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	note E_, 1
+	note C_, 2
+	note G_, 1
+	note E_, 1
+	note C_, 1
+	note C_, 1
+	note C#, 1
+	note F_, 1
+	note C#, 2
+	note G#, 1
+	note B_, 1
+	note C#, 1
+	octave 4
+	note D_, 3
+	note D_, 3
+	note G_, 4
+	note G_, 3
+	note G_, 3
+	note E_, 2
+	note C_, 2
+	octave 3
+	note G_, 1
+	note E_, 2
+	note A_, 1
+	note A#, 1
+	note A#, 1
+	note A_, 1
+	note A_, 1
+	note G#, 1
+	note G#, 1
+	note G_, 1
+	note G_, 1
+	note_type 12, 11, 3
+	note D#, 1
+	note E_, 2
+	note E_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 2
+	note C_, 1
+	note D_, 1
+	note D#, 2
+	note D#, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 2
+	note C_, 1
+	note F#, 1
+	note G_, 2
+	note G_, 3
+	note G_, 1
+	note A_, 2
+	note A#, 1
+	note F#, 1
+	note G_, 1
+	note D#, 1
+	note E_, 1
+	note C_, 1
+	note C#, 2
+	note E_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	octave 2
+	note G#, 1
+	note A_, 1
+	note E_, 1
+	note F_, 1
+	octave 3
+	note D#, 2
+	note D#, 1
+	note C_, 2
+	note D#, 1
+	note C_, 2
+	note_type 12, 11, 2
+	pitch_offset 2
+	note D#, 1
+	note E_, 2
+	note D#, 1
+	note E_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+	octave 2
+	note G_, 1
+	note F#, 1
+	note G_, 1
+	octave 3
+	note C_, 1
+	octave 2
+	note F#, 1
+	note G_, 1
+	octave 3
+	note C_, 1
+	octave 2
+	note F#, 1
+	note G_, 1
+	octave 3
+	note F#, 1
+	note G_, 2
+	note F#, 1
+	note G_, 1
+	note B_, 1
+	sound_call Music_OreburghCity_Ch1_sub_1
+	note F#, 1
+	note G_, 1
+	sound_call Music_OreburghCity_Ch1_sub_1
+	note G#, 1
+	note A_, 1
+	octave 4
+	note C_, 1
+	octave 3
+	note G#, 1
+	note A_, 1
+	octave 4
+	note C_, 1
+	octave 3
+	note G#, 1
+	note A_, 1
+	octave 4
+	note C_, 1
+	note C_, 1
+	octave 3
+	note E_, 1
+	note F_, 1
+	note A_, 1
+	note C_, 1
+	note E_, 1
+	note F_, 2
+	octave 2
+	note A#, 1
+	note A#, 2
+	note A#, 1
+	note A#, 2
+	note A#, 1
+	octave 3
+	note D#, 2
 	note D#, 1
 	note D#, 2
-	intensity $B3
-	note C_, 7
-	intensity $B1
-	octave 2
-	note G_, 2
-	note G_, 1
+	note D#, 1
+	note D#, 1
+	note D#, 1
+	sound_loop 0, .loop
+Music_OreburghCity_Ch2_sub_0:
+	note C_, 2
+	octave 4
+	note A_, 3
 	note G_, 3
-	octave 3
-	note G_, 2
-	note G_, 1
-	note G_, 3
-	note G_, 2
-	loopchannel 0, Music_OreburghCity_Ch1_loop
-
-Music_OreburghCity_Ch2:
-	tone $0001
-	dutycycle 1
-Music_OreburghCity_Ch2_loop:
-	notetype $C, $C5
+	note C_, 2
+	note D#, 2
+	note D_, 2
+	note C_, 1
+	sound_ret
+Music_OreburghCity_Ch2::
+	duty_cycle 3
+	vibrato 16, 3, 6
+	stereo_panning FALSE, TRUE
+.loop
+	note_type 12, 12, 5
 	octave 4
 	note C_, 3
 	note G_, 3
@@ -168,14 +245,7 @@ Music_OreburghCity_Ch2_loop:
 	note F_, 2
 	note G_, 2
 	octave 5
-	note C_, 2
-	octave 4
-	note A_, 3
-	note G_, 3
-	note C_, 2
-	note D#, 2
-	note D_, 2
-	note C_, 1
+	sound_call Music_OreburghCity_Ch2_sub_0
 	note D_, 3
 	note C_, 3
 	note G_, 3
@@ -184,30 +254,19 @@ Music_OreburghCity_Ch2_loop:
 	note G_, 2
 	octave 5
 	note D_, 2
-	note C_, 2
-	octave 4
-	note A_, 3
-	note G_, 3
-	note C_, 2
-	note D#, 2
-	note D_, 2
-	note C_, 1
+	sound_call Music_OreburghCity_Ch2_sub_0
 	note G#, 3
-	intensity $C3
+	note_type 12, 12, 2
 	note G_, 3
 	note G_, 3
 	note F#, 4
-	intensity $C1
 	note F_, 2
 	note F_, 4
-	intensity $C3
 	note E_, 3
 	note E_, 3
 	note E_, 4
-	intensity $C1
 	note F_, 2
 	note F_, 4
-	intensity $C3
 	note G_, 3
 	note G_, 3
 	octave 5
@@ -228,22 +287,20 @@ Music_OreburghCity_Ch2_loop:
 	note D_, 1
 	note C#, 1
 	note C#, 1
-	intensity $C5
+	note_type 12, 12, 5
 	note C_, 3
 	octave 3
 	note A_, 3
 	octave 4
 	note C_, 2
-	note D_, 1
-	note D_, 1
+	note D_, 2
 	note C_, 2
 	note D#, 1
 	note C_, 3
 	note E_, 3
 	note D#, 3
 	note E_, 2
-	note A#, 1
-	note A#, 1
+	note A#, 2
 	note A_, 2
 	note G#, 1
 	note A_, 3
@@ -256,7 +313,7 @@ Music_OreburghCity_Ch2_loop:
 	note C_, 2
 	note D_, 1
 	note C_, 3
-	intensity $C2
+	note_type 12, 12, 3
 	note C_, 2
 	note C_, 2
 	note D_, 1
@@ -266,9 +323,7 @@ Music_OreburghCity_Ch2_loop:
 	octave 4
 	note C_, 1
 	note D_, 1
-	intensity $C3
 	note C_, 5
-	intensity $C2
 	note E_, 2
 	note E_, 2
 	note D#, 1
@@ -306,13 +361,8 @@ Music_OreburghCity_Ch2_loop:
 	note G_, 2
 	note G_, 1
 	note G_, 2
-	loopchannel 0, Music_OreburghCity_Ch2_loop
-
-Music_OreburghCity_Ch3:
-Music_OreburghCity_Ch3_loop:
-	notetype $C, $12
-	octave 2
-	note C_, 1
+	sound_loop 0, .loop
+Music_OreburghCity_Ch3_sub_0:
 	note C_, 1
 	note E_, 1
 	note E_, 1
@@ -349,82 +399,66 @@ Music_OreburghCity_Ch3_loop:
 	note F_, 1
 	note G#, 1
 	note F_, 1
+	sound_ret
+Music_OreburghCity_Ch3_sub_1:
+	note C_, 1
+	rest 2
 	note C_, 1
 	note C_, 1
-	note E_, 1
-	note E_, 1
-	note G_, 1
-	note G_, 1
-	note A#, 1
-	note G_, 1
-	note E_, 1
-	note E_, 1
-	note G_, 1
-	note G_, 1
-	note A#, 1
-	note A#, 1
-	octave 3
-	note D_, 1
+	rest 2
+	note C_, 1
+	rest 1
+	note C_, 1
+	note C_, 1
+	rest 5
+	sound_ret
+Music_OreburghCity_Ch3::
+	note_type 12, 1, 4
+.loop
 	octave 2
-	note A#, 1
-	note F_, 1
-	note F_, 1
-	note A_, 1
-	note A_, 1
-	octave 3
 	note C_, 1
+	sound_call Music_OreburghCity_Ch3_sub_0
 	note C_, 1
-	note E_, 1
-	note C_, 1
-	octave 1
-	note A#, 1
-	note A#, 1
-	octave 2
+	sound_call Music_OreburghCity_Ch3_sub_0
 	note D_, 1
+	rest 2
 	note D_, 1
-	note F_, 1
-	note F_, 1
-	note G#, 1
-	note F_, 1
+	rest 2
 	note D_, 1
-	note __, 2
-	note D_, 1
-	note __, 2
-	note D_, 1
-	note __, 3
+	rest 3
 	note G_, 1
-	note __, 1
+	rest 1
 	note G_, 1
-	note __, 3
+	rest 3
 	note C_, 1
-	note __, 2
+	rest 2
 	note C_, 1
-	note __, 2
+	rest 2
 	note C_, 1
-	note __, 3
+	rest 3
 	note C#, 1
-	note __, 1
+	rest 1
 	note C#, 1
-	note __, 4
+	rest 4
 	note D_, 1
-	note __, 1
+	rest 1
 	note D_, 1
 	note D_, 1
-	note __, 3
+	rest 3
 	note G_, 1
-	note __, 1
+	rest 1
 	note G_, 1
 	note G_, 1
-	note __, 5
+	rest 5
 	note C_, 1
-	note __, 1
+	rest 1
 	note C_, 1
 	note C_, 1
-	note __, 4
+	rest 4
 	note G_, 1
-	note __, 2
+	rest 2
 	note A#, 1
-	note __, 3
+	rest 3
 	note F_, 1
 	note F_, 1
 	note A_, 1
@@ -484,238 +518,139 @@ Music_OreburghCity_Ch3_loop:
 	note C_, 1
 	note D#, 1
 	note C_, 1
-	note __, 1
+	rest 1
 	octave 2
-	note C_, 1
-	note __, 2
-	note C_, 1
-	note C_, 1
-	note __, 2
-	note C_, 1
-	note __, 1
-	note C_, 1
-	note C_, 1
-	note __, 5
-	note C_, 1
-	note __, 2
-	note C_, 1
-	note C_, 1
-	note __, 2
-	note C_, 1
-	note __, 1
-	note C_, 1
-	note C_, 1
-	note __, 5
+	sound_call Music_OreburghCity_Ch3_sub_1
+	sound_call Music_OreburghCity_Ch3_sub_1
 	note F_, 1
-	note __, 2
+	rest 2
 	note F_, 1
 	note F_, 1
-	note __, 2
+	rest 2
 	note F_, 1
-	note __, 1
+	rest 1
 	note F_, 1
 	note F_, 1
-	note __, 5
+	rest 5
 	note C_, 1
-	note __, 2
+	rest 2
 	note C_, 1
 	note C_, 1
-	note __, 2
+	rest 2
 	note G_, 1
-	note __, 1
+	rest 1
 	note G_, 1
 	note G_, 1
-	note __, 4
-	loopchannel 0, Music_OreburghCity_Ch3_loop
-
-Music_OreburghCity_Ch4:
-	togglenoise 3
-Music_OreburghCity_Ch4_loop:
-	notetype $C
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note C_, 2
-	note E_, 1
-	note C_, 2
-	note E_, 1
-	note C_, 2
-	note E_, 2
-	note C_, 2
-	note C_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 1
-	note C_, 2
-	note E_, 1
-	note C_, 2
-	note E_, 2
-	note C_, 2
-	note C_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 1
-	note C_, 2
-	note E_, 1
-	note C_, 2
-	note C_, 1
-	note G#, 2
-	note C_, 1
-	note C_, 2
-	note G#, 1
-	note C_, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note C_, 1
-	note C_, 2
-	note C_, 1
-	note C_, 1
-	note C_, 2
-	note C_, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note A#, 1
-	note G#, 1
-	note C_, 1
-	note G#, 1
-	note C_, 1
-	note G#, 2
-	note A#, 1
-	note A#, 2
-	note C_, 2
-	note A#, 1
-	note C_, 3
-	note A#, 2
-	note C_, 2
-	note A#, 1
-	note C_, 3
-	note A#, 2
-	note C_, 2
-	note A#, 1
-	note C_, 3
-	note A#, 2
-	note C_, 2
-	note A#, 1
-	note C_, 3
-	note A#, 2
-	note C_, 2
-	note A#, 1
-	note C_, 3
-	note A#, 2
-	note C_, 2
-	note A#, 1
-	note C_, 3
-	note A#, 2
-	note C_, 2
-	note A#, 1
-	note C_, 3
-	note A#, 2
-	note C_, 2
-	note A#, 1
-	note C_, 3
-	loopchannel 0, Music_OreburghCity_Ch4_loop
+	rest 4
+	sound_loop 0, .loop
+Music_OreburghCity_Ch4_sub_0:
+	drum_note 2, 2
+	drum_note 8, 2
+	drum_note 2, 2
+	drum_note 8, 2
+	drum_note 2, 2
+	drum_note 8, 2
+	drum_note 2, 2
+	drum_note 8, 2
+	sound_ret
+Music_OreburghCity_Ch4_sub_1:
+	drum_note 7, 2
+	drum_note 2, 1
+	drum_note 7, 2
+	drum_note 2, 1
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 8, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	sound_ret
+Music_OreburghCity_Ch4_sub_2:
+	drum_note 2, 2
+	drum_note 8, 1
+	drum_note 7, 1
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 8, 1
+	drum_note 7, 1
+	drum_note 2, 2
+	drum_note 7, 2
+	sound_ret
+Music_OreburghCity_Ch4_sub_3:
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 8, 2
+	drum_note 7, 1
+	drum_note 8, 2
+	drum_note 7, 1
+	drum_note 8, 2
+	sound_ret
+Music_OreburghCity_Ch4::
+	toggle_noise 3
+	drum_speed 12
+.loop
+	sound_call Music_OreburghCity_Ch4_sub_0
+	sound_call Music_OreburghCity_Ch4_sub_0
+	sound_call Music_OreburghCity_Ch4_sub_0
+	sound_call Music_OreburghCity_Ch4_sub_0
+	sound_call Music_OreburghCity_Ch4_sub_1
+	sound_call Music_OreburghCity_Ch4_sub_1
+	drum_note 7, 2
+	drum_note 2, 1
+	drum_note 7, 2
+	drum_note 2, 1
+	drum_note 7, 2
+	drum_note 8, 2
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 2
+	drum_note 8, 1
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 1
+	sound_call Music_OreburghCity_Ch4_sub_2
+	sound_call Music_OreburghCity_Ch4_sub_2
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 7, 1
+	drum_note 2, 2
+	drum_note 2, 1
+	sound_call Music_OreburghCity_Ch4_sub_3
+	sound_call Music_OreburghCity_Ch4_sub_3
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 7, 1
+	drum_note 2, 2
+	drum_note 2, 1
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 2, 2
+	drum_note 7, 2
+	drum_note 7, 1
+	drum_note 7, 1
+	drum_note 2, 1
+	drum_note 7, 2
+	drum_note 2, 1
+	drum_note 7, 1
+	drum_note 7, 1
+	sound_loop 0, .loop
