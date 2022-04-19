1390fac0b258b2b57cbac1a356b3507bd32df8c9
U-TASTYSNAX12-X1-\TastySnax12 <tastysnax12@gmail.com>
2022-04-18 21:57:06 -0400

    Implemented Aroma Lady sprite.

--- data/trainers/palettes.asm
+++ data/trainers/palettes.asm
@@ -39,7 +39,7 @@ INCBIN "gfx/trainers/pokemaniac.gbcpal", middle_colors
 INCBIN "gfx/trainers/grunt_m.gbcpal", middle_colors
 INCBIN "gfx/trainers/gentleman.gbcpal", middle_colors
 INCBIN "gfx/trainers/skier.gbcpal", middle_colors
-INCBIN "gfx/trainers/teacher.gbcpal", middle_colors
+INCBIN "gfx/trainers/aroma_lady.gbcpal", middle_colors
 INCBIN "gfx/trainers/sabrina.gbcpal", middle_colors
 INCBIN "gfx/trainers/bug_catcher.gbcpal", middle_colors
 INCBIN "gfx/trainers/fisher.gbcpal", middle_colors
